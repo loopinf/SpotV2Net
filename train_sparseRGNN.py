da955ab280f69101f3101ec7c6fa4a1d20b788aa
Alessiobrini <alessiobrini@gmail.com>
2023-05-18 15:09:03 -0400

    Optimized batch training

--- train_sparseRGNN.py
+++ train_sparseRGNN.py
@@ -20,7 +20,7 @@ import yaml
 
 
 class CovarianceTemporalDataset(InMemoryDataset):
-    def __init__(self, hdf5_file, root='processed_data/cached_datasets_temporal/', transform=None, pre_transform=None, seq_length=None):
+    def __init__(self, hdf5_file, root='processed_data/cached_datasets_temporal_zeroentries/', transform=None, pre_transform=None, seq_length=None):
         self.hdf5_file = hdf5_file
         self.root = root
         self.seq_length = seq_length
@@ -73,13 +73,14 @@ class CovarianceTemporalDataset(InMemoryDataset):
                     # Extract only upper triangle of the next adjacency matrix (excluding diagonal)
                     mask = np.triu(np.ones_like(adj_matrix_next), k=1) > 0
                     y_edge_weight = torch.tensor(adj_matrix_next[mask], dtype=torch.float).view(-1, 1)
+                    y_binary = torch.where(y_edge_weight != 0, torch.tensor(1.0), torch.tensor(0.0))
     
-                    # Extract the variances (diagonal) of the next covariance matrix as target node features
-                    y_x = torch.tensor(np.diag(next_cov_matrix), dtype=torch.float).view(-1, 1)
                     
                     # Create PyTorch Geometric Data object
-                    data = Data(x=x, edge_index=edge_index, edge_weight=edge_weights,
-                                y_edge_weight=y_edge_weight, y_x=y_x)
+                    data = Data(x=x, edge_index=edge_index, 
+                                edge_weight=edge_weights,
+                                y_edge_weight=y_edge_weight,
+                                y_binary=y_binary)
                     seq_data_list.append(data)
     
                 # Combine the sequence of Data objects into a single object with the desired format
@@ -87,11 +88,11 @@ class CovarianceTemporalDataset(InMemoryDataset):
                                 edge_index=torch.stack([d.edge_index for d in seq_data_list], dim=0),
                                 edge_weight=torch.stack([d.edge_weight for d in seq_data_list], dim=0),
                                 y_edge_weight=torch.stack([d.y_edge_weight for d in seq_data_list], dim=0),
-                                y_x=torch.stack([d.y_x for d in seq_data_list], dim=0))
+                                y_binary=torch.stack([d.y_binary for d in seq_data_list], dim=0))
                 
     
                 data_list.append(seq_data)
-
+    
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
 
@@ -104,65 +105,53 @@ class GATModel(torch.nn.Module):
     def __init__(self, num_features, hidden_channels, num_heads, output_edge_channels, output_node_channels, seq_length):
         super(GATModel, self).__init__()
         self.seq_length = seq_length
-        self.num_features = num_features
         self.gat1 = GATConv(num_features, hidden_channels, heads=num_heads, concat=True, dropout=0.6)
         self.gat2 = GATConv(hidden_channels * num_heads, hidden_channels, heads=num_heads, concat=True, dropout=0.6)
         self.lstm = nn.LSTM(hidden_channels * num_heads, hidden_channels * num_heads, batch_first=True)
         self.edge_weight_predictor = torch.nn.Linear(hidden_channels * num_heads, output_edge_channels)
-        self.node_feature_predictor = torch.nn.Linear(hidden_channels * num_heads, output_node_channels)
+        self.edge_classifier = torch.nn.Linear(hidden_channels * num_heads, 1)
         # Apply Xavier initialization to the linear layers
         torch.nn.init.xavier_uniform_(self.edge_weight_predictor.weight)
-        torch.nn.init.xavier_uniform_(self.node_feature_predictor.weight)
+        torch.nn.init.xavier_uniform_(self.edge_classifier.weight)
 
         
         
-    def forward(self, data, batch_size):
+    def forward(self, data):
         batches_temporal_embedding = []
-        
-        # reshape to merge batch dimension and node dimension
-        x, edge_index, edge_weights = (data.x.reshape(self.seq_length,-1, self.num_features),
-                                       data.edge_index,
-                                       data.edge_weight.reshape(self.seq_length,-1))
-        
-        temporal_embedding = []
-        for j in range(x.shape[0]):
-            # GAT layers
-            h = self.gat1(x[j], edge_index[j], edge_weights[j])
-            h = F.relu(h)
-            h = F.dropout(h, p=0.6, training=self.training)
-
-            h = self.gat2(h, edge_index[j], edge_weights[j])
-            h = F.relu(h)
-            temporal_embedding.append(h)
-        batches_temporal_embedding.append(temporal_embedding)
+        # iterating over batches
+        for i in range(len(data)):
+            temporal_embedding = []
+            x, edge_index, edge_weights = data[i].x, data[i].edge_index, data[i].edge_weight
+            for j in range(x.shape[0]):
+                # GAT layers
+                h = self.gat1(x[j], edge_index[j], edge_weights[j])
+                h = F.relu(h)
+                h = F.dropout(h, p=0.6, training=self.training)
+    
+                h = self.gat2(h, edge_index[j], edge_weights[j])
+                h = F.relu(h)
+                temporal_embedding.append(h)
+            batches_temporal_embedding.append(temporal_embedding)
         
         batches_temporal_embedding = torch.stack([torch.cat(inner_list, dim=0) for inner_list in batches_temporal_embedding], dim=0)
         # Reshape x to (batch_size*seq_length,nodes, hidden_features)
         x = batches_temporal_embedding.reshape(batches_temporal_embedding.shape[0] * self.seq_length, 
-                                                -1, 
-                                                batches_temporal_embedding.shape[-1])
+                                               -1, 
+                                               batches_temporal_embedding.shape[-1])
         x, _ = self.lstm(x)
         x = x[::self.seq_length,:,:]
+        
+    
         x = x.reshape(x.shape[0]*x.shape[1],-1)
         
         edge_weights_pred = self.edge_weight_predictor(x[data.edge_index[0][0]] * x[data.edge_index[0][1]])
-        # Predict node features for each node separately
-        node_features_pred = self.node_feature_predictor(x)
 
-        return edge_weights_pred, node_features_pred
+        classification_logits = self.edge_classifier(x[data.edge_index[0][0]] * x[data.edge_index[0][1]])
+        classification_logits = classification_logits.squeeze(-1)
 
+        return edge_weights_pred, classification_logits
 
-def back_to_matcov(y,y_x):
-    # Initialize the covariance matrix with zeros
-    cov = torch.diag(y_x.reshape(-1,))
-    # Create a mask for the upper triangle of the covariance matrix
-    mask = torch.triu(torch.ones_like(cov), diagonal=1).bool()
-    # Fill the upper triangle of the covariance matrix with the covariances from edge_weights_pred
-    cov[mask] = y.flatten()
-    # Mirror the upper triangle to the lower triangle to make the matrix symmetric
-    cov = cov + cov.T - torch.diag(cov.diag())
     
-    return cov
 
 
 if __name__ == '__main__':
@@ -171,25 +160,19 @@ if __name__ == '__main__':
     with open('config/GNN_param.yaml', 'r') as f:
         p = yaml.safe_load(f)
     # Define the folder path
-    folder_path = 'output/{}_{}'.format(p['modelname'],p['seq_length'])
+    folder_path = 'output/{}'.format(p['modelname'])
     
-    if p['train'] == True:
-        # Check if the folder exists, and create it if it doesn't
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-        
-        # Save the yaml file to the model folder
-        with open('{}/GNN_param.yaml'.format(folder_path), 'w') as f:
-            yaml.dump(p, f)
-    else:
-        # Load hyperparam file
-        with open('{}/GNN_param.yaml'.format(folder_path), 'r') as f:
-            p = yaml.safe_load(f)
-        p['train'] = False
+    # Check if the folder exists, and create it if it doesn't
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    
+    # Save the yaml file to the model folder
+    with open('{}/GNN_param.yaml'.format(folder_path), 'w') as f:
+        yaml.dump(p, f)
 
     # Instantiate the dataset
-    dataset = CovarianceTemporalDataset(hdf5_file=p['datafile'],root='_'.join([p['root'],str(p['seq_length'])]), seq_length=p['seq_length'])
-    
+    dataset = CovarianceTemporalDataset(hdf5_file=p['datafile'], seq_length=p['seq_length'])
+
     # train-test split data
     train_size = int(p['split_proportion'] * len(dataset))
     test_size = len(dataset) - train_size
@@ -218,11 +201,11 @@ if __name__ == '__main__':
         for epoch in range(p['num_epochs']):
             model.train()
             total_loss = 0
-            
             for data in tqdm(iterable=train_loader, desc='Training batches...'):
                 data = data.to(device)
+                pdb.set_trace()
                 # Forward pass
-                edge_weights_pred, node_features_pred = model(data,p['batch_size'])
+                edge_weights_pred, node_features_pred = model(data)
                 # Compute loss
                 loss_edge_weights = criterion(edge_weights_pred, data.y_edge_weight[::p['seq_length'],:,:].reshape(edge_weights_pred.shape))
                 loss_node_features = criterion(node_features_pred, data.y_x[::p['seq_length'],:,:].reshape(node_features_pred.shape))
@@ -238,7 +221,7 @@ if __name__ == '__main__':
             avg_train_loss = total_loss / len(train_loader)
             
             # Check if the average training loss improved by the specified tolerance
-            if epoch == 0 or avg_train_loss + float(p['tolerance']) < prev_train_loss:
+            if epoch == 0 or avg_train_loss + p['tolerance'] < prev_train_loss:
                 # Update the previous training loss
                 prev_train_loss = avg_train_loss
     
@@ -255,7 +238,7 @@ if __name__ == '__main__':
                     data = data.to(device)
         
                     # Forward pass
-                    edge_weights_pred, node_features_pred = model(data,p['batch_size'])
+                    edge_weights_pred, node_features_pred = model(data)
                     # Compute loss
                     loss_edge_weights = criterion(edge_weights_pred, data.y_edge_weight[::p['seq_length'],:,:].reshape(edge_weights_pred.shape))
                     loss_node_features = criterion(node_features_pred, data.y_x[::p['seq_length'],:,:].reshape(node_features_pred.shape))
@@ -273,9 +256,10 @@ if __name__ == '__main__':
         
         # Load saved model weights
         modelweights = os.path.join(folder_path,'{}_weights.pth'.format(p['modelname']))
-        model.load_state_dict(torch.load(modelweights, map_location=device))
+        model.load_state_dict(torch.load(modelweights))
         # Set loss function and optimizer
         # criterion = torch.nn.MSELoss()
+        
         # Evaluate on the test set
         model.eval()
 
@@ -283,25 +267,28 @@ if __name__ == '__main__':
             for data in tqdm(iterable=test_loader, desc='Testing batches...'):
                 data = data.to(device)
                 # Forward pass
-                edge_weights_pred, node_features_pred = model(data,p['batch_size'])
-                edge_weights_pred, node_features_pred = (edge_weights_pred.reshape(p['batch_size'],-1,1), 
-                                                         node_features_pred.reshape(p['batch_size'],-1,1))
+                edge_weights_pred, node_features_pred = model(data)
                 
-                y, y_x = (data.y_edge_weight.reshape(p['batch_size'],-1,1)[:,::p['seq_length'],:], 
-                          data.y_x.reshape(p['batch_size'],-1,1)[:,::p['seq_length'],:])
-
-                for i in range(p['batch_size']):
-                    cov_hat = back_to_matcov(edge_weights_pred[i],node_features_pred[i])
-                    cov = back_to_matcov(y[i],y_x[i])
-                    
-                    mse = torch.mean((cov_hat-cov)**2)
-                    mae = torch.mean((cov_hat-cov))
-                    
-                    print('MSE',mse)
-                    print('MAe',mae)
-                    print()
-                    
-                    # check
-                    # cov.equal(cov.t())
-
+                # Initialize the covariance matrix with zeros
+                cov = torch.diag(node_features_pred.reshape(-1,))
+                # Create a mask for the upper triangle of the covariance matrix
+                mask = torch.triu(torch.ones_like(cov), diagonal=1).bool()
+                # Fill the upper triangle of the covariance matrix with the covariances from edge_weights_pred
+                cov[mask] = edge_weights_pred.flatten()
+                # Mirror the upper triangle to the lower triangle to make the matrix symmetric
+                cov = cov + cov.T - torch.diag(cov.diag())
+                # check
+                # cov.equal(cov.t())
+                pdb.set_trace()
                 
+        
+        #         # Compute loss
+        #         loss_edge_weights = criterion(edge_weights_pred, data.y_edge_weight[::p['seq_length'],:,:].reshape(edge_weights_pred.shape))
+        #         loss_node_features = criterion(node_features_pred, data.y_x[::p['seq_length'],:,:].reshape(node_features_pred.shape))
+        #         loss = loss_edge_weights + loss_node_features
+        #         test_loss += loss.item()
+        
+        # # Compute average test loss
+        # avg_test_loss = test_loss / len(test_loader)
+        
+        # print(f"Test Loss: {avg_test_loss:.10f}")
